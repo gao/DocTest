4aa8ebc74d40f6f85f5e2e5ae0ed4f64a05e1d29
Gao Bin <bin_gao@live.cn>
2010-12-01 20:39:02 +0800

    modify the DocManager

--- src/com/n2napps/doctest/manager/DocManager.java
+++ src/com/n2napps/doctest/manager/DocManager.java
@@ -5,10 +5,30 @@ import java.util.List;
 
 import com.n2napps.doctest.FieldValues;
 
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+
 
 public class DocManager {
     
-    public void getFields(File docxFile){
+    public List<FieldValues> getFields(File docxFile){
+        
+        try {
+            //To load a document
+            WordprocessingMLPackage wordMLPackage =  WordprocessingMLPackage.load(docxFile);
+            
+            //get the main document part
+            MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+            
+            
+            
+        } catch (Docx4JException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
+        return null;
         
     }
     
