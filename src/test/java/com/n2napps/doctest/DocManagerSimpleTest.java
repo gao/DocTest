f8676fbbb442a35ccab0d75f76abd0a80dc85a6e
Gao Bin <bin_gao@live.cn>
2010-12-02 15:44:02 +0800

    modify the DocManagerSimpleTest add the test for fillAndSave

--- src/test/java/com/n2napps/doctest/DocManagerSimpleTest.java
+++ src/test/java/com/n2napps/doctest/DocManagerSimpleTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.docx4j.openpackaging.exceptions.Docx4JException;
@@ -23,11 +24,38 @@ public class DocManagerSimpleTest {
       DocManager manager = new DocManager();
       
       //get fields
-      List<Field> values = manager.getFields(inputFile);
+      List<Field> fileds = manager.getFields(inputFile);
       
-      assertEquals("values size is 2",2, values.size());
-      assertEquals("one field is name",values.get(0).getName(),"name");
-      assertEquals("other field is address",values.get(1).getName(),"address");
+      assertEquals("fileds size is 2",2, fileds.size());
+      assertEquals("one field is name",fileds.get(0).getName(),"name");
+      assertEquals("other field is address",fileds.get(1).getName(),"address");
+    }
+    
+    @Test
+    public void testFillAndSave() throws Docx4JException {
+      String inputFileString = "src/test/resources/DocManager/01_simple.docx";
+      String outputFileString = "src/test/resources/DocManager/output/01_simple_test.docx";
+      
+      File inputFile = new File(inputFileString);
+      File outputFile = new File(outputFileString);
+      DocManager manager = new DocManager();
+      
+      //get fields
+      List<Field> fileds = manager.getFields(inputFile);
+      
+      assertEquals("values size is 2",2, fileds.size());
+      assertEquals("one field is name",fileds.get(0).getName(),"name");
+      assertEquals("other field is address",fileds.get(1).getName(),"address");
+      
+      List<FieldValues> fieldvalues = new ArrayList<FieldValues>();
+      for(Field filed : fileds){
+          FieldValues fv = new FieldValues();
+          fv.setName(filed.getName());
+          fv.setValue(filed.getName()+"value");
+          fieldvalues.add(fv);
+      }
+
+      manager.fillAndSave(inputFile, outputFile, fieldvalues);
     }
 
 }
