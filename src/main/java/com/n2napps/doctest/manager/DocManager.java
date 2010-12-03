967edfebe19111ec8dc744568c5a74523fc74df5
Gao Bin <bin_gao@live.cn>
2010-12-03 18:04:19 +0800

    add the FormManager

--- src/main/java/com/n2napps/doctest/manager/DocManager.java
+++ src/main/java/com/n2napps/doctest/manager/DocManager.java
@@ -16,232 +16,13 @@ import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.wml.Body;
 import org.docx4j.wml.Text;
 
-import com.n2napps.doctest.Field;
-import com.n2napps.doctest.FieldValues;
 
 
 public class DocManager {
     
-public List<Field> getFields(File docxFile) throws Docx4JException{
-    	
-    	final List<Field> fields = new ArrayList<Field>();
-    	
-    	WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(docxFile);
-		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
-
-		org.docx4j.wml.Document wmlDocumentEl = (org.docx4j.wml.Document) documentPart.getJaxbElement();
-		Body body = wmlDocumentEl.getBody();
-		
-		
-		new TraversalUtil(body,
-
-			new Callback() {
-	
-				List<Text> textList = new ArrayList();
-				// Depth first
-				@Override
-				public void walkJAXBElements(Object parent) {
-	
-	
-					List children = getChildren(parent);
-					if (children != null) {
-						for (Object o : children) {
-							// if its wrapped in javax.xml.bind.JAXBElement, get its
-							// value
-							o = XmlUtils.unwrap(o);
-	
-							this.apply(o);
-	
-							if (this.shouldTraverse(o)) {
-								walkJAXBElements(o);
-							}
-	
-						}
-					}
-	
-				}
-	
-				@Override
-				public List<Object> getChildren(Object o) {
-					return TraversalUtil.getChildrenImpl(o);
-				}
-				
-				@Override
-				public List<Object> apply(Object o) {
-	
-					if (o instanceof org.docx4j.wml.Text){
-						Text text = (org.docx4j.wml.Text) o;
-						if(text.getValue().indexOf("$")>=0){
-							textList.clear();
-							textList.add(text);
-							if(text.getValue().indexOf("}")>=0){
-								String fieldName = getFieldName(textList);
-								if(!checkFieldName(fieldName,fields)){
-								    Field f = new Field();
-	                                f.setName(fieldName);
-								    fields.add(f);
-								}
-								textList.clear();
-							}
-						}else if(textList.size()>0){
-							textList.add(text);
-							if(text.getValue().indexOf("}")>=0){
-								String fieldName = getFieldName(textList);
-								if(!checkFieldName(fieldName,fields)){
-                                    Field f = new Field();
-                                    f.setName(fieldName);
-                                    fields.add(f);
-                                }
-								textList.clear();
-							}
-							
-						}
-					}
-					return null;
-				}
-	
-				@Override
-				public boolean shouldTraverse(Object o) {
-					return true;
-				}
-				
-				private String getFieldName(List<Text> textList){
-					StringBuilder fieldName = new StringBuilder();
-					for(Text t : textList){
-						fieldName.append(t.getValue());
-					}
-					return fieldName.substring(2, fieldName.length()-1);
-				}
-				
-				private boolean checkFieldName(String filedName,List<Field> fields){
-                    boolean isHave = false;
-				    for(int i=0;i<fields.size();i++){
-				        if(fields.get(i).getName().equals(filedName)){
-				            isHave = true;
-				        }
-                    }
-				    return isHave;
-                }
-	
-			}
-
-		);
-		return fields;
-     }
-    
-    public void fillAndSave(File docxFile, File destinationDocxFile, final List<FieldValues> values) throws Docx4JException{
-		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(docxFile);
-		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
-
-		org.docx4j.wml.Document wmlDocumentEl = (org.docx4j.wml.Document) documentPart.getJaxbElement();
-		Body body = wmlDocumentEl.getBody();
-		
-		
-		new TraversalUtil(body,
-
-			new Callback() {
-	
-				List<Text> textList = new ArrayList();
-				// Depth first
-				@Override
-				public void walkJAXBElements(Object parent) {
-	
-	
-					List children = getChildren(parent);
-					if (children != null) {
-						for (Object o : children) {
-							// if its wrapped in javax.xml.bind.JAXBElement, get its
-							// value
-							o = XmlUtils.unwrap(o);
-	
-							this.apply(o);
-	
-							if (this.shouldTraverse(o)) {
-								walkJAXBElements(o);
-							}
-	
-						}
-					}
-	
-				}
-	
-				@Override
-				public List<Object> getChildren(Object o) {
-					return TraversalUtil.getChildrenImpl(o);
-				}
-				
-				@Override
-				public List<Object> apply(Object o) {
-	
-					if (o instanceof org.docx4j.wml.Text){
-						Text text = (org.docx4j.wml.Text) o;
-						if(text.getValue().indexOf("$")>=0){
-							textList.clear();
-							textList.add(text);
-							if(text.getValue().indexOf("}")>=0){
-								int i = 0;
-								String value = getFieldValue(values,getFieldName(textList));
-								for(Text t : textList){
-									if(i==0){
-										t.setValue("  " + value + "  ");
-									}else{
-										t.setValue("");
-									}
-									i++;
-								}
-								textList.clear();
-							}
-						}else if(textList.size()>0){
-							textList.add(text);
-							if(text.getValue().indexOf("}")>=0){
-								int i = 0;
-								String value = getFieldValue(values,getFieldName(textList));
-								for(Text t : textList){
-									if(i==0){
-										t.setValue("  " + value + "  ");
-									}else{
-										t.setValue("");
-									}
-									i++;
-								}
-								textList.clear();
-							}
-						}
-					}
-					return null;
-				}
-	
-				@Override
-				public boolean shouldTraverse(Object o) {
-					return true;
-				}
-				
-				private String getFieldValue(List<FieldValues> fieldValues, String name){
-					for(FieldValues field : fieldValues){
-						if(field.getName().equals(name)){
-							return field.getValue();
-						}
-					}
-					return "";
-				}
-
-				private String getFieldName(List<Text> textList){
-					StringBuilder fieldName = new StringBuilder();
-					for(Text t : textList){
-						fieldName.append(t.getValue());
-					}
-					return fieldName.substring(2, fieldName.length()-1);
-				}
-			}
-
-		);
-		
-		wordMLPackage.save(destinationDocxFile);
-     }
     
     public void docxToHtml(File docxFile,String outputFileString) throws Exception{                 
         // Load .docx
-        //this need : commons-io-1.3.1.jar commons-logging-1.1.1.jar serializer-2.7.1.jar xalan-2.7.1.jar
         WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(docxFile); 
         AbstractHtmlExporter exporter = new HtmlExporterNG2();                        
                           
@@ -251,5 +32,87 @@ public List<Field> getFields(File docxFile) throws Docx4JException{
         exporter.html(wordMLPackage, result,outputFileString+"test_files"); 
         
     }
+    
+    
+//    public void replace(File docxFile,final String textToReplace,final String newText) throws Docx4JException{
+//        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(docxFile);
+//        MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+//
+//        org.docx4j.wml.Document wmlDocumentEl = (org.docx4j.wml.Document) documentPart.getJaxbElement();
+//        Body body = wmlDocumentEl.getBody();
+//    
+//    
+//        new TraversalUtil(body,
+//            new Callback() {
+//
+//                List<Text> textList = new ArrayList();
+//                // Depth first
+//                @Override
+//                public void walkJAXBElements(Object parent) {
+//
+//                    List children = getChildren(parent);
+//                    if (children != null) {
+//                        for (Object o : children) {
+//                            o = XmlUtils.unwrap(o);
+//
+//                            this.apply(o);
+//
+//                            if (this.shouldTraverse(o)) {
+//                                walkJAXBElements(o);
+//                            }
+//                        }
+//                    }
+//                }
+//
+//                @Override
+//                public List<Object> getChildren(Object o) {
+//                    return TraversalUtil.getChildrenImpl(o);
+//                }
+//            
+//                @Override
+//                public List<Object> apply(Object o) {
+//                    if (o instanceof org.docx4j.wml.Text){
+//                        Text text = (org.docx4j.wml.Text) o;
+//                        String textStr = text.getValue();
+//                        int testIndexofValue = textStr.indexOf(textToReplace.charAt(0));
+//                        if(testIndexofValue >= 0){
+//                            textStr = textStr.substring(testIndexofValue);
+//                            if(textStr.length() == textToReplace.length()){
+//                                if(textStr.equals(textToReplace)){
+//                                    textList.clear();
+//                                    textList.add(text);
+//                                    int i = 0;
+//                                    for(Text t : textList){
+//                                        if(i==0){
+//                                            t.setValue("  " + newText + "  ");
+//                                        }else{
+//                                            t.setValue("");
+//                                        }
+//                                        i++;
+//                                    }
+//                                    textList.clear();
+//                                }
+//                            }else if(textStr.length() > textToReplace.length()){
+//                                textStr = textStr.substring(testIndexofValue,textToReplace.length());
+//                                
+//                            }else{
+//                                
+//                            }
+//                        }
+//                    }
+//                    return null;
+//                }
+//
+//                @Override
+//                public boolean shouldTraverse(Object o) {
+//                    return true;
+//                }
+//            
+//            }
+//
+//        );
+//    
+//        wordMLPackage.save(docxFile);
+//    }
 
 }
---
+++ src/test/java/com/n2napps/doctest/FormManagerSimpleTest.java
@@ -0,0 +1,107 @@
+package com.n2napps.doctest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.junit.Test;
+
+import com.n2napps.doctest.manager.FormManager;
+
+public class FormManagerSimpleTest {
+    
+    //use the file 01_simple.docx,have ${name} and ${address}
+    @Test
+    public void testGetFields() throws Docx4JException {
+      String inputFileString = "src/test/resources/DocManager/01_simple.docx";
+      
+      File inputFile = new File(inputFileString);
+      FormManager manager = new FormManager();
+      
+      //get fields
+      List<Field> fileds = manager.getFields(inputFile);
+      
+      assertEquals("fileds size is 2",2, fileds.size());
+      assertEquals("one field is name",fileds.get(0).getName(),"name");
+      assertEquals("other field is address",fileds.get(1).getName(),"address");
+    }
+    
+
+    @Test
+    public void testFillAndSave() throws Docx4JException {
+      String inputFileString = "src/test/resources/DocManager/01_simple.docx";
+      String outputFileString = "src/test/resources/DocManager/output/01_simple_test.docx";
+      
+      File inputFile = new File(inputFileString);
+      File outputFile = new File(outputFileString);
+      FormManager manager = new FormManager();
+      
+      //get fields
+      List<Field> fileds = manager.getFields(inputFile);
+      
+      assertEquals("values size is 2",2, fileds.size());
+      assertEquals("one field is name",fileds.get(0).getName(),"name");
+      assertEquals("other field is address",fileds.get(1).getName(),"address");
+      
+      List<FieldValues> fieldvalues = new ArrayList<FieldValues>();
+      for(Field filed : fileds){
+          FieldValues fv = new FieldValues();
+          fv.setName(filed.getName());
+          fv.setValue(filed.getName()+"value");
+          fieldvalues.add(fv);
+      }
+
+      manager.fillAndSave(inputFile, outputFile, fieldvalues);
+    }
+    
+    
+    //use the file 02_redundant_field.docx,have 2 ${name} 2 ${address} and 1 ${phone}
+    @Test
+    public void testGetFieldsHaveSameField() throws Docx4JException {
+      String inputFileString = "src/test/resources/DocManager/02_redundant_field.docx";
+      
+      File inputFile = new File(inputFileString);
+      FormManager manager = new FormManager();
+      
+      //get fields
+      List<Field> fileds = manager.getFields(inputFile);
+      
+      assertEquals("fileds size is 3",3, fileds.size());
+      assertEquals("one field is name",fileds.get(0).getName(),"name");
+      assertEquals("other field is address",fileds.get(1).getName(),"address");
+      assertEquals("other field is phone",fileds.get(2).getName(),"phone");
+    }
+    
+    
+    @Test
+    public void testFillAndSaveHaveSameField() throws Docx4JException {
+      String inputFileString = "src/test/resources/DocManager/02_redundant_field.docx";
+      String outputFileString = "src/test/resources/DocManager/output/02_redundant_field_test.docx";
+      
+      File inputFile = new File(inputFileString);
+      File outputFile = new File(outputFileString);
+      FormManager manager = new FormManager();
+      
+      //get fields
+      List<Field> fileds = manager.getFields(inputFile);
+      
+      assertEquals("fileds size is 3",3, fileds.size());
+      assertEquals("one field is name",fileds.get(0).getName(),"name");
+      assertEquals("other field is address",fileds.get(1).getName(),"address");
+      assertEquals("other field is phone",fileds.get(2).getName(),"phone");
+      
+      List<FieldValues> fieldvalues = new ArrayList<FieldValues>();
+      for(Field filed : fileds){
+          FieldValues fv = new FieldValues();
+          fv.setName(filed.getName());
+          fv.setValue(filed.getName()+"value");
+          fieldvalues.add(fv);
+      }
+
+      manager.fillAndSave(inputFile, outputFile, fieldvalues);
+    }
+
+}
