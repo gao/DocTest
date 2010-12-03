816388ef0a43c576d82cd468f7f9298435ee0475
Gao Bin <bin_gao@live.cn>
2010-12-03 22:36:34 +0800

    add the replace method in DocManager

--- src/main/java/com/n2napps/doctest/manager/DocManager.java
+++ src/main/java/com/n2napps/doctest/manager/DocManager.java
@@ -3,7 +3,10 @@ package com.n2napps.doctest.manager;
 import java.io.File;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+
+import javax.xml.bind.JAXBException;
 
 import org.docx4j.TraversalUtil;
 import org.docx4j.XmlUtils;
@@ -11,10 +14,13 @@ import org.docx4j.TraversalUtil.Callback;
 import org.docx4j.convert.out.html.AbstractHtmlExporter;
 import org.docx4j.convert.out.html.HtmlExporterNG2;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
+import org.docx4j.openpackaging.io.SaveToZipFile;
 import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
 import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
 import org.docx4j.wml.Body;
+import org.docx4j.wml.Document;
 import org.docx4j.wml.Text;
+import org.junit.Test;
 
 
 
@@ -34,85 +40,35 @@ public class DocManager {
     }
     
     
-//    public void replace(File docxFile,final String textToReplace,final String newText) throws Docx4JException{
-//        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(docxFile);
-//        MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
-//
-//        org.docx4j.wml.Document wmlDocumentEl = (org.docx4j.wml.Document) documentPart.getJaxbElement();
-//        Body body = wmlDocumentEl.getBody();
-//    
-//    
-//        new TraversalUtil(body,
-//            new Callback() {
-//
-//                List<Text> textList = new ArrayList();
-//                // Depth first
-//                @Override
-//                public void walkJAXBElements(Object parent) {
-//
-//                    List children = getChildren(parent);
-//                    if (children != null) {
-//                        for (Object o : children) {
-//                            o = XmlUtils.unwrap(o);
-//
-//                            this.apply(o);
-//
-//                            if (this.shouldTraverse(o)) {
-//                                walkJAXBElements(o);
-//                            }
-//                        }
-//                    }
-//                }
-//
-//                @Override
-//                public List<Object> getChildren(Object o) {
-//                    return TraversalUtil.getChildrenImpl(o);
-//                }
-//            
-//                @Override
-//                public List<Object> apply(Object o) {
-//                    if (o instanceof org.docx4j.wml.Text){
-//                        Text text = (org.docx4j.wml.Text) o;
-//                        String textStr = text.getValue();
-//                        int testIndexofValue = textStr.indexOf(textToReplace.charAt(0));
-//                        if(testIndexofValue >= 0){
-//                            textStr = textStr.substring(testIndexofValue);
-//                            if(textStr.length() == textToReplace.length()){
-//                                if(textStr.equals(textToReplace)){
-//                                    textList.clear();
-//                                    textList.add(text);
-//                                    int i = 0;
-//                                    for(Text t : textList){
-//                                        if(i==0){
-//                                            t.setValue("  " + newText + "  ");
-//                                        }else{
-//                                            t.setValue("");
-//                                        }
-//                                        i++;
-//                                    }
-//                                    textList.clear();
-//                                }
-//                            }else if(textStr.length() > textToReplace.length()){
-//                                textStr = textStr.substring(testIndexofValue,textToReplace.length());
-//                                
-//                            }else{
-//                                
-//                            }
-//                        }
-//                    }
-//                    return null;
-//                }
-//
-//                @Override
-//                public boolean shouldTraverse(Object o) {
-//                    return true;
-//                }
-//            
-//            }
-//
-//        );
-//    
-//        wordMLPackage.save(docxFile);
-//    }
+    public void replace(File docxFile,String outputFileString ,List<String> textToReplace,List<String> newText) throws Docx4JException, JAXBException{
+        boolean save = true;
+        WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(docxFile);           
+        MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+        org.docx4j.wml.Document wmlDocumentEl = (org.docx4j.wml.Document) documentPart.getJaxbElement(); 
+                            
+        //xml --> string
+        String xml = XmlUtils.marshaltoString(wmlDocumentEl, true);
+            
+        HashMap<String, String> mappings = new HashMap<String, String>();
+            
+        for(int i=0;i<textToReplace.size();i++){
+            mappings.put(textToReplace.get(i), newText.get(i));
+        }
+          
+        //valorize template
+        Object obj = XmlUtils.unmarshallFromTemplate(xml, mappings);
+            
+        //change  JaxbElement
+        documentPart.setJaxbElement((Document) obj);
+
+        // Save it        
+        if (save) {     
+            SaveToZipFile saver = new SaveToZipFile(wordMLPackage);
+            saver.save(outputFileString);
+            System.out.println( "Saved output to:" + outputFileString );
+        } else {
+            // Display the Main Document Part.
+        }
+    }
 
 }
--- src/test/java/com/n2napps/doctest/DocManagerSimpleTest.java
+++ src/test/java/com/n2napps/doctest/DocManagerSimpleTest.java
@@ -2,6 +2,15 @@ package com.n2napps.doctest;
 
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.docx4j.XmlUtils;
+import org.docx4j.openpackaging.io.SaveToZipFile;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.MainDocumentPart;
+import org.docx4j.wml.Document;
 import org.junit.Test;
 
 import com.n2napps.doctest.manager.DocManager;
@@ -19,6 +28,26 @@ public class DocManagerSimpleTest {
       //docx to html
       manager.docxToHtml(inputFile,outputFileString);
       
+    }
+    
+    
+    @Test
+    public void testReplace() throws Exception {
+        String inputFileString = "src/test/resources/DocManager/03_replace.docx";
+        String outputFileString = "src/test/resources/DocManager/output/03_replace_test.docx";
+      
+        File inputFile = new File(inputFileString);
+        DocManager manager = new DocManager();
+        
+        List<String> textToReplace = new ArrayList<String>();
+        textToReplace.add("colour");
+        textToReplace.add("icecream");
+        
+        List<String> newText = new ArrayList<String>();
+        newText.add("green");
+        newText.add("chocolate");
+        
+        manager.replace(inputFile, outputFileString, textToReplace, newText);
     }
 
 }
